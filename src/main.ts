d88d0a1ba228125d557f9c784a3f996ffe7853b7
shichiye <shichiye@126.com>
2022-04-22 21:01:57 +0800

    feat(base.css): 初始化基础css

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { createApp } from 'vue'
 
 import App from './App.vue'
 
+import 'normalize.css'
+import './assets/css/index.less'
 import router from './router'
 import store from './store'
 import 'element-plus/theme-chalk/base.css'
