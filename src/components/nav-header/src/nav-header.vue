6a7be006c32b4eb1dd822bca6a58e335c10a235c
shichiye <shichiye@126.com>
2022-05-25 13:50:30 +0800

    新增用户信息组件

--- src/components/nav-header/src/nav-header.vue
+++ src/components/nav-header/src/nav-header.vue
@@ -3,11 +3,16 @@
     <el-icon class="nav-header_icon" @click="handleFoldClick" :size="25">
       <component :is="isFold ? 'Expand' : 'Fold'"></component>
     </el-icon>
+    <div class="nav-header_content">
+      <div>面包屑</div>
+      <user-info />
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue'
+import UserInfo from './user-info.vue'
 
 const emit = defineEmits(['foldChange'])
 
@@ -20,7 +25,20 @@ const handleFoldClick = () => {
 </script>
 
 <style lang="less" scoped>
+.nav-header {
+  display: flex;
+  width: 100%;
+}
+
 .nav-header_icon {
   cursor: pointer;
+}
+
+.nav-header_content {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex: 1;
+  padding: 0 20px;
 }
 </style>
---
+++ src/components/nav-header/src/user-info.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="user-info">
+    <el-dropdown>
+      <span class="user-info_dropdown">
+        <el-avatar
+          :size="30"
+          class="user-info_dropdown_avatar"
+          src="https://th.bing.com/th/id/R.8daa7ee26391a412a0b0d2b2b6bc7a46?rik=SBMaDD8FCe7YCg&riu=http%3a%2f%2f5b0988e595225.cdn.sohucs.com%2fimages%2f20180508%2f8a2bf96052b54dedbbfc4433f7adfe75.jpeg&ehk=jI7dC9niTeEDZCxQV%2fr5JdOOfCpcapYQJeKOZZOujNQ%3d&risl=&pid=ImgRaw&r=0"
+        />
+        <span>{{ name }}</span>
+      </span>
+      <template #dropdown>
+        <el-dropdown-menu>
+          <el-dropdown-item icon="avatar">用户信息</el-dropdown-item>
+          <el-dropdown-item icon="operation">系统管理</el-dropdown-item>
+          <el-dropdown-item divided icon="close">退出登录</el-dropdown-item>
+        </el-dropdown-menu>
+      </template>
+    </el-dropdown>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue'
+import { useStore } from '@/store'
+
+const store = useStore()
+
+const name = computed(() => store.state.login.userInfo.name)
+</script>
+
+<style lang="less" scoped>
+.user-info_dropdown {
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+
+  &_avatar {
+    margin-right: 10px;
+  }
+}
+</style>
